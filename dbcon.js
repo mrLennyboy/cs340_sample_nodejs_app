bd7ed18190dd2c821d0aed03afc73ff8eb3893e5
Jasper <wongjasp@oregonstate.edu>
2019-11-21 14:43:09 -0800

    Update dbcon.js
    With Jasper access info

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_wolfordj',
-  password        : '7235',
-  database        : 'cs340_wolfordj'
+  user            : 'cs340_wongjasp',
+  password        : '8994',
+  database        : 'cs340_wongjasp'
 });
 module.exports.pool = pool;
