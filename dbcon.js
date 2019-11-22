e916130264a9f71e554aeec511b293b14d18cb17
alessio-peterson <52301333+alessio-peterson@users.noreply.github.com>
2019-11-22 04:32:34 -0800

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_wongjasp',
-  password        : '8994',
-  database        : 'cs340_wongjasp'
+  user            : 'cs340_peteales',
+  password        : '0044',
+  database        : 'cs340_peteales'
 });
 module.exports.pool = pool;
