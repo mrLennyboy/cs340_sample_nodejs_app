480c4e984705698a736aa8f099cc70a5ec17302e
Jasper <wongjasp@oregonstate.edu>
2019-12-07 15:23:55 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
-var express = require('express');
-var mysql = require('./dbcon.js');
-var bodyParser = require('body-parser');
+/*******************************************************************************
+** Author: Alessio Peterson and Jasper Wong
+** Program Name: CS340 Final Project
+** Course: CS 340-400
+** Comment: This code was modified and provided by J.Wolford from instruction
+**          materials.
+*******************************************************************************/
 
+//Include express to use for webapp
+var express = require('express');
 var app = express();
+
+//SQL connection to database
+var mysql = require('./dbcon.js');
+app.set('mysql', mysql);
+
+//Include bodyParser and set express to use it
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//Express handlebars as default engine for handing handlebar files
 var handlebars = require('express-handlebars').create({
     defaultLayout: 'main',
 });
-
 app.engine('handlebars', handlebars.engine);
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/static', express.static('public'));
 app.set('view engine', 'handlebars');
+
+//Assume folder static documents in public
+app.use('/static', express.static('public'));
+
+//Set app listening port
 app.set('port', process.argv[2]);
-app.set('mysql', mysql);
+
 app.use('/donors', require('./donors.js'));
 app.use('/clients', require('./clients.js'));
 app.use('/dog_breed', require('./dog_breed.js'));
@@ -20,6 +39,7 @@ app.use('/dogs', require('./dogs.js'));
 app.use('/cats', require('./cats.js'));
 app.use('/', express.static('public'));
 
+//Error messages
 app.use(function (req, res) {
     res.status(404);
     res.render('404');
@@ -31,6 +51,7 @@ app.use(function (err, req, res, next) {
     res.render('500');
 });
 
+//Message when program called
 app.listen(app.get('port'), function () {
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
